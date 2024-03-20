fc2c8dda35f5b180a1609b4c33eb9498b843b69d
Llewellyn Paintsil <llewellynpaintsil34@gmail.com>
2024-03-20 01:41:59 +0000

    Refactor footer component and update global styles

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="bg-[#269DA3] text-white py-5 mb-5">
-  <div className="container mx-auto flex justify-between items-center px-0">
-    <span className="font-bold text-2xl">MediWhisper</span>
-    <span className="font-bold text-2xl">© 2024</span>
-  </div>
-</footer>
+    <footer className="self-stretch bg-[#269DA3] overflow-hidden flex flex-row items-start justify-between py-[1.75rem] px-[1.5rem] gap-[1.25rem] z-[2] text-left text-[1.563rem] text-white font-poppins mq450:flex-wrap">
+        <div className="w-[10.5rem] relative leading-[104.69%] font-semibold inline-block mq450:text-[1.25rem] mq450:leading-[1.313rem]">
+          MediWhisper
+        </div>
+        <div className="w-[5.438rem] relative leading-[104.69%] font-semibold inline-block mq450:text-[1.25rem] mq450:leading-[1.313rem]">
+          © 2024
+        </div>
+      </footer>
   );
 }
 
