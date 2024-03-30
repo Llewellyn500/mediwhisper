e14d1e585f1a8f730cc762f399e3ee34e13a8cba
Llewellyn Paintsil <llewellynpaintsil34@gmail.com>
2024-03-30 21:47:49 +0000

    redid footer component

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="self-stretch bg-[#269DA3] overflow-hidden flex flex-row items-start justify-between py-[1.75rem] px-[1.5rem] gap-[1.25rem] z-[2] text-left text-[1.563rem] text-white mq450:flex-wrap z-[1]">
-        <div className="w-[10.5rem] relative leading-[104.69%] font-semibold inline-block mq450:text-[1.25rem] mq450:leading-[1.313rem]">
+    <footer className="self-stretch rounded-xl bg-[#269DA3] overflow-hidden flex flex-row items-start justify-between py-[1.75rem] px-[1.5rem] gap-[1.25rem] text-left text-[1.563rem] text-white font-poppins mq450:flex-wrap">
+        <div className="relative leading-[104.69%] font-semibold mq450:text-[1.25rem] mq450:leading-[1.313rem]">
           MediWhisper
         </div>
-        <div className="w-[5.438rem] relative leading-[104.69%] font-semibold inline-block mq450:text-[1.25rem] mq450:leading-[1.313rem]">
+        <div className="relative leading-[104.69%] font-semibold inline-block min-w-[5.438rem] mq450:text-[1.25rem] mq450:leading-[1.313rem]">
           © 2024
         </div>
       </footer>
