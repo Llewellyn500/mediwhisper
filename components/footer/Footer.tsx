cd65949f8aea17aac70330bdefbb1a649c0ee81c
Llewellyn Paintsil <llewellynpaintsil34@gmail.com>
2024-03-30 21:50:33 +0000

    change footer font

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="self-stretch rounded-xl bg-[#269DA3] overflow-hidden flex flex-row items-start justify-between py-[1.75rem] px-[1.5rem] gap-[1.25rem] text-left text-[1.563rem] text-white font-poppins mq450:flex-wrap">
+    <footer className="self-stretch rounded-xl bg-[#269DA3] overflow-hidden flex flex-row items-start justify-between py-[1.75rem] px-[1.5rem] gap-[1.25rem] text-left text-[1.563rem] text-white font-lato mq450:flex-wrap">
         <div className="relative leading-[104.69%] font-semibold mq450:text-[1.25rem] mq450:leading-[1.313rem]">
           MediWhisper
         </div>
